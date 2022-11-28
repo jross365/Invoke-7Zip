42ff548e6fda1db0a7717c418a86ec9e95f0def2
jross365 <jross365github@gmail.com>
2022-11-28 15:11:57 -0500

    Added "Escape" key for extract; var. improvements

--- Invoke-7Zip.ps1
+++ Invoke-7Zip.ps1
@@ -27,7 +27,7 @@ Else {$Target = $FileSystemObject.GetAbsolutePathName($Target)}
 
 Remove-Variable FileSystemObject | Out-null
 
-#endregion Case-Correct
+#endregion Case-correct the File/Directory
 
 #region Validate File/Directory parameters, based on switches
 If ($Add.IsPresent){
@@ -35,21 +35,21 @@ If ($Add.IsPresent){
     try {!($TestPath = Test-Path $ArchiveFile -ErrorAction Stop)}
     catch {throw "$ArchiveFile already exists, stopping"}
 
-} #Close if $Add.IsPresent
+} 
 
 If ($Extract.IsPresent -or $List.IsPresent){
 
     try {$TestPath = Test-Path $ArchiveFile -ErrorAction Stop}
     catch {throw "$ArchiveFile doesn't exist, stopping"}
 
-    } #Close If $Extract.IsPresent|$List.IsPresent
+    } 
 
 If ($Add.IsPresent -or $Extract.IsPresent){
 
     try {$TestPath = Test-Path $Target -ErrorAction Stop}
     catch {throw "$Target doesn't exist, stopping"}
 
-} #Close If $Add.IsPresent|$Extract.IsPresent
+} 
 
 #endregion Validate Exists
 
@@ -131,17 +131,24 @@ Else {throw "No valid parameters were found"}
 #7z x test.7z -bsp1 -ppassword -mmt14 -bse0 2>&1 >test.txt
 #Extract test.7z, Redirect Progress to Stream 1, Password "password", 14 threads, Redirect Error to Stream 0, Redirect Stream 2 to 1, Output console output to test.txt
 
+#endregion Build Params
+
+#region Build Logfile
+
 If ($Operation -eq "Add" -or $Operation -eq "Extract"){
 
-$LogFile = "$((Get-Location).Path)\$(get-random -Minimum 1000000 -Maximum 9999999).log"
+    $LogFile = "$((Get-Location).Path)\$(get-random -Minimum 1000000 -Maximum 9999999).log"
+    
+    try {"Write test" | Out-File $LogFile -ErrorAction Stop}
+    catch {throw "Unable to write data out to new logfile $LogFile"}
 
-try {"Write test" | Out-File $LogFile -ErrorAction Stop}
-catch {throw "Unable to write data out to new logfile $LogFile"}
-#endregion Build Params
+$LogfileCleanup = {If (!($KeepLogfile.IsPresent)){Remove-Item $LogFile -ErrorAction SilentlyContinue}}
+
+#endregion Build Logfile
 
 #region Build Scriptblock
 
-$Scriptblock = { #11/13: working on building this to capture console output
+$Scriptblock = {
 
     $WorkingDirectory = $args[0]
     $7zPath = $args[1]
@@ -157,121 +164,164 @@ $Scriptblock = { #11/13: working on building this to capture console output
 
     return $RunCommand
 
-    } #Close Scriptblock
+    }
 
 } #Close if $Operation -eq Add|Extract
 
 #endregion Build Scriptblock
+
+#region Build ESCAPE key Interception
+$Global:Interrupted = $False #We have to use Global
+
+#This blocks CTRL+C and flushes the input buffer
+$InitializeInputBuffer = {
+    [Console]::TreatControlCAsInput = $True
+    Start-Sleep -Milliseconds 100
+    $Host.UI.RawUI.FlushInputBuffer()
+}
+
+$InterceptEscapeKey = {
+    
+    If ($Host.UI.RawUI.KeyAvailable -and ($Key = $Host.UI.RawUI.ReadKey("AllowCtrlC,NoEcho,IncludeKeyUp"))) {
+
+    If ([Int]$Key.Character -eq 27) {
+        
+        Write-Warning "Escape Key detected, terminating operation (please wait)"
+        $Global:Interrupted = $True
+        [Console]::TreatControlCAsInput = $False
+
+    }
+
+    $Host.UI.RawUI.FlushInputBuffer()
+}
+
+} #Close $InterceptEscapeKey
+
+#endregion Build ESCAPE key Interception
 
 } #Close Begin
 
 process {
 
 <#Explanation of the Following:
- We need the "Technical Info" of the archive for "Extract" to determine whether
+ We need the list "Technical Info" of the archive for "Extract" to determine whether
  the archive is encrypted, and to enumerate the smallest file in the archive to
  test the provided password before attempting to unpack the archive.
 
- Therefore, "-List -ShowTechnicalInfo" and "-Extract" both require the "l -slt" parameter.
+ Therefore, "-Extract" requires the "l -slt" output.
 #>
-#If ($Operation -ne "Add" -and $Operation -ne "List"){
+
 If ($Operation -eq "Extract" -or $Operation -eq "ListSLT"){
+    
     $TechContents = 7z l "$ArchiveFile" -slt 2>&1
     
 #region TechContents Error-catching:
-    $ArchiveErrors = $TechContents[$TechContents.GetUpperBound(0)..0].Where({$_.Exception.Message.Length -gt 0}).Exception.Message
+    $ArchiveErrors = $TechContents.Where({$_.Exception.Message.Length -gt 0}).Exception.Message
     
     Switch ($ArchiveErrors.Count){
     
     0 {$EncryptedTags = $TechContents.Where({$_ -eq "Encrypted = +"}) | Sort-Object -Unique}
     
-    1 {throw $ArchiveErrors}
+    1 {
+        &$LogfileCleanup
+        throw $ArchiveErrors
+    }
     
-    {$_ -gt 1}{$ArchiveErrors[($ArchiveErrors.Count -1)..1].ForEach{(Write-Error -Message "$_" -ErrorAction Continue)}; throw ($ArchiveErrors[0])}
+    {$_ -gt 1}{
+        &$LogfileCleanup
+        $ArchiveErrors[($ArchiveErrors.Count -1)..1].ForEach{(Write-Error -Message "$_" -ErrorAction Continue)}
+        throw ($ArchiveErrors[0])
+    }
     
-    Default {throw "No idea what happened"}
+    Default {
+        &$LogfileCleanup
+        throw "No idea what happened"
+    }
     
     } #Close Switch ArchiveErrors.Count
 #endregion TechContents Error-catching
     
-#region Test Password for encrypted volumes
-    #If ($null -ne $EncryptedTags -and $null -eq $Password -and !($List.IsPresent)){throw "Archive is encrypted, but no password was specified"}
+#region Build Technical Info List, Test Password
     If ($Operation -eq "Extract" -and $EncryptedTags.Count -gt 0 -and ($null -eq $Password -or $Password.Length -eq 0)){throw "Archive is encrypted, but no password was specified"}
+        
+    $TechTable = New-Object System.Collections.ArrayList
 
-    If ($Operation -eq "Extract" -or $Operation -eq "ListSLT"){
-    
-        #Build a Powershell Array with parsed -slt data
-        $TechTable = New-Object System.Collections.ArrayList
+    $HeaderBreak = $TechContents.IndexOf("----------")
+    $FileInfoLines = $TechContents[$HeaderBreak..$TechContents.GetUpperBound(0)].Count
 
-        $HeaderBreak = $TechContents.IndexOf("----------")
-        $FileInfoLines = $TechContents[$HeaderBreak..$TechContents.GetUpperBound(0)].Count
+    $x = $HeaderBreak + 1
+    Do {  
 
-        $x = $HeaderBreak + 1
-        Do {  
+        $LineLength = 1
+        $y = $x
 
-                $LineLength = 1
-                $y = $x
+        Do {
 
-                Do {
-
-                    $LineLength = $TechContents[$y].Length
-                    $y++
-
-                }
-                Until ($LineLength -eq 0)
-
-                $StartIndex = $x
-                $EndIndex = $y - 2 #-2 because the "Until" statement doesn't evaluate until after $y++
-
-                $Object = New-Object System.Object
-                $TechContents[$StartIndex..$EndIndex].ForEach({
-
-                    $LineSplit = ($_ -split ' = ', 2).ForEach({$_.Trim()})
-                    $Object | Add-Member -MemberType NoteProperty -Name ($LineSplit[0]) -Value ($LineSplit[1])
-
-                    })
-
-                $TechTable.Add($Object) | Out-Null
-                
-                $x = $y 
+            $LineLength = $TechContents[$y].Length
+            $y++
 
         }
-        Until ($x -ge $FileInfoLines)
+        Until ($LineLength -eq 0)
 
-        #region Test the Password
-        If ($Operation -eq "Extract" -and $null -ne $Password -and $null -ne $EncryptedTags){ #If we're not doing l -slt, we need to find the smallest file to test password against
+        $StartIndex = $x
+        $EndIndex = $y - 2 #-2 because the "Until" statement doesn't evaluate until after $y++
 
-        $SmallestFile = $TechTable.Where({$_.Encrypted -eq '+'}) | Sort-Object Size | Select-Object -First 1
+        $Object = New-Object System.Object
+        $TechContents[$StartIndex..$EndIndex].ForEach({
+
+            $LineSplit = ($_ -split ' = ', 2).ForEach({$_.Trim()})
+            $Object | Add-Member -MemberType NoteProperty -Name ($LineSplit[0]) -Value ($LineSplit[1])
+
+            })
+
+        $TechTable.Add($Object) | Out-Null
         
-        #There seems to be a bug in 7z where -i!\<Path> tests more than the specified file. No idea why, but it's a non-issue even if it hurts performance.
-        $PasswordTest = 7z t $ArchiveFile -p"$Password" -i!\$($SmallestFile.Path) -bso0 -bd 2>&1
+        $x = $y 
+
+    }
+    Until ($x -ge $FileInfoLines)
+
+    #region Test the Password
+    If ($Operation -eq "Extract" -and $null -ne $Password -and $null -ne $EncryptedTags){ 
+
+    #Find the smallest file to test password against
+    $SmallestFile = $TechTable.Where({$_.Encrypted -eq '+'}) | Sort-Object Size | Select-Object -First 1
+    
+    #There seems to be a 7zip bug where -i!\<Path> tests more than the specified file. No idea why, but it's not solvable via Powershell
+    $PasswordTest = 7z t $ArchiveFile -p"$Password" -i!\$($SmallestFile.Path) -bso0 -bd 2>&1
+            
+    If ($PasswordTest.Count -gt 0){$TestErrors = $PasswordTest.Where({$_.Exception.Message.Length -gt 0}).Exception.Message}
+
+    Switch ($TestErrors.Count){
+
+        0 {} #Do nothing
+
+        1 {
+            &$LogfileCleanup
+            throw $TestErrors
+        }
+
+        {$_ -gt 1}{
+            &$LogfileCleanup
+            $TestErrors.ForEach{(Write-Error -Message "$_" -ErrorAction Continue)}
+            throw ($TestErrors[0])
+        }
+
+        Default {
+            &$LogfileCleanup
+            throw "No idea what happened"
+        }
+
+        }
+
+    }
+    #endregion Test the Password
         
-        #If ($PasswordTest.Count -gt 0){$TestErrors = $PasswordTest[$PasswordTest.GetUpperBound(0)..0].Where({$_.Exception.Message.Length -gt 0}).Exception.Message}
-        If ($PasswordTest.Count -gt 0){$TestErrors = $PasswordTest.Where({$_.Exception.Message.Length -gt 0}).Exception.Message}
+    } #Close If ($Operation -eq "Extract" -or $Operation -eq "ListSLT")
 
-        Switch ($TestErrors.Count){
-    
-            0 {} #Do nothing
-    
-            1 {throw $TestErrors}
-    
-            #{$_ -gt 1}{$TestErrors[($TestErrors.GetUpperBound(0))..1].ForEach{(Write-Error -Message "$_" -ErrorAction Continue)}; throw ($TestErrors[0])}
-            {$_ -gt 1}{$TestErrors.ForEach{(Write-Error -Message "$_" -ErrorAction Continue)}; throw ($TestErrors[0])}
+#endregion Build Technical Info List, Test Password
 
-            Default {throw "No idea what happened"}
-    
-            } #Close Switch TestErrors.Count
-    
-        } #Close if $Operation -ne ListSLT
-        #endregion Test the Password
-
-    } #Close If $null -ne $encryptedtags -and $null -ne $password, -or $Operation -eq "ListSLT"
-    
-    } #Close If $Operation -ne "Add"|"List"
-
-    #endregion Test Password for encrypted volumes
-
-#region Do the actual work
+#region (Heavy-lifting)
 
 Switch ($Operation){
 
@@ -287,11 +337,11 @@ Switch ($Operation){
     
             0 {} #Do nothing
     
-            1 {throw $ListErrors}
+            1 {&$LogfileCleanup; throw $ListErrors}
     
-            {$_ -gt 1}{$ListErrors[($ListErrors.Count -1)..1].ForEach{(Write-Error -Message "$_" -ErrorAction Continue)}; throw ($ListErrors[0])}
+            {$_ -gt 1}{&$LogfileCleanup; $ListErrors[($ListErrors.Count -1)..1].ForEach{(Write-Error -Message "$_" -ErrorAction Continue)}; throw ($ListErrors[0])}
     
-            Default {throw "No idea what happened"}
+            Default {&$LogfileCleanup; throw "No idea what happened"}
     
         } #Close Switch TestErrors.Count
 
@@ -351,7 +401,7 @@ Switch ($Operation){
 
             If ($BreakTable[$Index].Name -match $DateTime){$BreakTable[$Index].Name = "DateTime"}
         })
-        #endregion Create a table
+        #endregion Create A Table
         
         #region Use the index points to parse out the -l contents        
         $ListOutput[($FirstBreakIndex + 1)..($LastBreakIndex - 1)].ForEach({
@@ -371,7 +421,7 @@ Switch ($Operation){
         $ListTable.Add($Object) | Out-Null
 
         }) #Close $ListOutput.ForEach()
-        #endregion
+        #endregion Create A Table
         
         } #Close if $_ -eq "List"
 
@@ -387,12 +437,15 @@ Switch ($Operation){
 
             Switch ($JobStatus.State){ #Check our job state to make sure it's running
 
-            {$_ -eq "Failed"}{throw "Job $($JobStatus.Id) failed for 7zip extract operation"}
-            {$_ -eq "Blocked"}{throw "Job $($JobStatus.Id) was blocked from running 7zip extract operation"}
-            {$_ -eq "Stopped"}{throw "Job $($JobStatus.Id) was stopped while running 7zip extract operation"}
-            {$_ -eq "Suspended"}{throw "Job $($JobStatus.Id) was suspended from running 7zip extract operation"}
+            {$_ -eq "Failed"}{&$LogfileCleanup; throw "Job $($JobStatus.Id) failed for 7zip extract operation"}
+            {$_ -eq "Blocked"}{&$LogfileCleanup; throw "Job $($JobStatus.Id) was blocked from running 7zip extract operation"}
+            {$_ -eq "Stopped"}{&$LogfileCleanup; throw "Job $($JobStatus.Id) was stopped while running 7zip extract operation"}
+            {$_ -eq "Suspended"}{&$LogfileCleanup; throw "Job $($JobStatus.Id) was suspended from running 7zip extract operation"}
 
             } #Close JobStatus.State
+
+            &$InitializeInputBuffer
+            Write-Verbose "Press ESCAPE key to interrupt extract operation" -Verbose
 
             Do {
 
@@ -423,10 +476,14 @@ Switch ($Operation){
 
                 Write-Progress -Activity "Extracting $ArchiveFile" -Status "$OnFile of $FileCount | Extracting $File" -PercentComplete $Percent
 
+                &$InterceptEscapeKey
+
             } #Close Do
-            Until ($Done -eq $True)
+            Until ($Done -eq $True -or $Global:Interrupted -eq $True)
 
             Write-Progress -Activity "Extracting $ArchiveFile" -Status "Ready" -Completed
+
+            If ($Global:Interrupted -eq $True){$Operation = "$Operation Interrupted"}
 
             $JobStatus = Get-Job ($Job.Id)
 
@@ -434,7 +491,7 @@ Switch ($Operation){
 
             {$_ -eq "Completed"}{$JobContents = ($Job | Receive-Job); $Job | Remove-Job}
 
-            {$_ -eq "Failed"}{throw "Job errored; $($JobStatus.Error)"}
+            {$_ -eq "Failed"}{&$LogfileCleanup; $JobError = $JobStatus.Error; $Job | Remove-Job; throw "Job errored: $JobError"}
 
             #11/19 - NEED TO CHECK JOB OUTPUT HERE
 
@@ -445,13 +502,15 @@ Switch ($Operation){
 
 } #Close switch $Operation
    
-#endregion Do the actual work
+#endregion (Heavy-lifting)
 
 } #Close Process
 
 end {
 
 Switch ($Operation){
+
+        {$_ -match 'Interrupted'}{Get-Job | Stop-Job; Get-Job | Remove-Job; &$LogfileCleanup; Write-Verbose "Operation $Operation before completing" -Verbose}
 
         {$_ -eq "ListSLT"}{return $TechTable}
 
@@ -461,11 +520,15 @@ Switch ($Operation){
 
             If ($KeepLogfile.IsPresent){Write-Verbose "Logfile is $LogFile" -Verbose}
 
+            &$LogfileCleanup
+
             } #Close If Eq Extract
 
         {$_ -eq "Add"}{
 
             If ($KeepLogfile.IsPresent){Write-Verbose "Logfile is $LogFile" -Verbose}
+
+            &$LogfileCleanup
         }
 
 } #Close Switch Operation
